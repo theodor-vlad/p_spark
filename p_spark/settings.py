c20915b10bc217915f3b22c747179a63f50f8f81
vtheo <vladtheodor0@gmail.com>
2025-01-12 20:48:38 +0200

    deleted static files

--- p_spark/settings.py
+++ p_spark/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,10 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "hello/static")]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
