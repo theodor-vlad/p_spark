e362dfe4306307509b001270dfdbfb10b6696a22
vtheo <vladtheodor0@gmail.com>
2025-01-12 19:18:33 +0200

    allowlisted some hosts in an attempt to make the app deployable to vercel

--- p_spark/settings.py
+++ p_spark/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-8(+qtn80xg-^e4ffpqxv74q4&vj1#=ri@dixyds@kw!*&*r12)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
